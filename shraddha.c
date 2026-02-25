3eeffed232f9fc9b526c240594095e60aff1cb5e
Md Arman <mdarmanali374@gmail.com>
2026-02-25 13:28:23 +0530

    -m "shraddha committed with c"

---
+++ shraddha.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter the value of a and b:");
+    scanf("%d%d",&a,&b);
+    printf("The value of a is %d and b is %d",a,b);
+    return 0;
+}
